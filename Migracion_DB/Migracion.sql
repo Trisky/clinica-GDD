1912285cd35abf82138d88bfaa52d6070d772e8c
Alejandro <alemattioli95@gmail.com>
2016-10-01 14:26:02 -0300

    Migracion medicos, sin finalizar, sequence e insert tipo doc

--- Migracion_DB/Migracion.sql
+++ Migracion_DB/Migracion.sql
@@ -1,3 +1,113 @@
+--===========================================================================================================
+--===========================================================================================================		   
+CREATE SEQUENCE GRUPOSA.SQ_ID_USUARIO  
+    START WITH 1   
+    INCREMENT BY 1;  
+
+--===========================================================================================================
+--===========================================================================================================
+--INSERT TIPOS DE DOCUMENTO
+INSERT INTO [GRUPOSA].[TipoDocumento]
+           (Tipo_Doc_Desc)
+     VALUES
+           ( 'DNI')
+
+INSERT INTO [GRUPOSA].[TipoDocumento]
+           (Tipo_Doc_Desc)
+	VALUES
+           ('PASAPORTE')
+
+INSERT INTO [GRUPOSA].[TipoDocumento]
+           (Tipo_Doc_Desc)
+     VALUES
+           ('LC')
+
+
+--===========================================================================================================
+--===========================================================================================================
+--/* Planes Medicos */
+
+USE GD2C2016
+BEGIN TRANSACTION
+	
+	DECLARE @plan_cod 					NUMERIC(18,0)
+	DECLARE @plan_desc					VARCHAR(255)
+	DECLARE @plan_precio_consulta 		NUMERIC(18,0)
+	DECLARE @plan_precio_bono_farm		NUMERIC(18,0)
+
+	DECLARE Cur_PlanesMedicos CURSOR FOR
+	
+		SELECT DISTINCT(plan_med_codigo), plan_med_descripcion,plan_med_precio_bono_consulta,plan_med_precio_bono_farmacia
+		FROM gd_esquema.Maestra
+		WHERE plan_med_codigo IS NOT NULL
+		ORDER BY 1 ASC
+
+	OPEN Cur_PlanesMedicos 
+		
+		FETCH Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm	
+	
+		WHILE (@@FETCH_STATUS = 0)
+
+			BEGIN	
+
+				INSERT INTO [GRUPOSA].[PlanesMedicos]
+				([Plan_Codigo],[Plan_Descripcion],[Plan_Precio_Bono_Consulta],[Plan_Precio_Bono_Farmacia])
+		   		VALUES
+				(@plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm);
+					
+				FETCH NEXT FROM Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm
+			END
+		
+
+	CLOSE Cur_PlanesMedicos
+
+	DEALLOCATE Cur_PlanesMedicos
+	
+COMMIT TRANSACTION
+--===========================================================================================================
+--===========================================================================================================
+--/* Especialidades Medicas */
+
+USE GD2C2016
+BEGIN TRANSACTION
+	
+	DECLARE @espe_cod 			NUMERIC(18,0)
+	DECLARE @espe_desc			VARCHAR(255)
+	DECLARE @espe_tipo_cod 		NUMERIC(18,0)
+	DECLARE @espe_tipo_desc		VARCHAR(255)
+
+	DECLARE Cur_Especialidades CURSOR FOR
+	
+		SELECT DISTINCT(especialidad_codigo), especialidad_descripcion, tipo_especialidad_codigo, tipo_especialidad_descripcion 
+		FROM gd_esquema.Maestra
+		WHERE especialidad_codigo IS NOT NULL
+		ORDER BY 1 ASC
+
+	OPEN Cur_Especialidades 
+		
+		FETCH Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
+		WHILE (@@FETCH_STATUS = 0)
+
+			BEGIN	
+
+				INSERT INTO [GRUPOSA].[Especialidades]
+				([Espe_Cod],[Espe_Desc],[Espe_Tipo_Cod],[Espe_Tipo_Desc])
+		   		VALUES
+				(@espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc);
+					
+				FETCH NEXT FROM Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
+			END
+		
+
+	CLOSE Cur_Especialidades
+
+	DEALLOCATE Cur_Especialidades
+	
+COMMIT TRANSACTION
+
+--===========================================================================================================
+--===========================================================================================================
+		   
 --1 creacion usuarios pacientes
 DECLARE @fecha DATETIME
 
@@ -41,107 +151,57 @@ Where Medico_Dni is not null
 
 --===========================================================================================================
 --===========================================================================================================
---/* 3 - Planes Medicos */
-
+--/* Medicos */
 USE GD2C2016
 BEGIN TRANSACTION
 	
-	DECLARE @plan_cod 					NUMERIC(18,0)
-	DECLARE @plan_desc					VARCHAR(255)
-	DECLARE @plan_precio_consulta 		NUMERIC(18,0)
-	DECLARE @plan_precio_bono_farm		NUMERIC(18,0)
+	DECLARE @medi_nom					VARCHAR(255)
+	DECLARE @medi_apell					VARCHAR(255)
+	DECLARE @medi_dni			 		NUMERIC(18,0)
+	DECLARE @medi_direccion				VARCHAR(255)
+	DECLARE @medi_mail 					VARCHAR(255)
+	DECLARE @medi_telefono				NUMERIC(18,0)
+	DECLARE @medi_fecha_nac				DATE
+	DECLARE @medi_especialidad			NUMERIC (18,0)
+	DECLARE @medi_tipodni				VARCHAR(255)
+	DECLARE @medi_usuario 				NUMERIC(18,0)
+	DECLARE @var1						NUMERIC(18,0)
 
-	DECLARE Cur_PlanesMedicos CURSOR FOR
+	DECLARE Cur_Medicos CURSOR FOR
 	
-		SELECT DISTINCT(plan_med_codigo), plan_med_descripcion,plan_med_precio_bono_consulta,plan_med_precio_bono_farmacia
-		FROM gd_esquema.Maestra
-		WHERE plan_med_codigo IS NOT NULL
-		ORDER BY 1 ASC
-
-	OPEN Cur_PlanesMedicos 
+		SELECT medico_nombre, medico_apellido, medico_dni, medico_direccion, medico_fecha_nac, medico_mail, medico_telefono, especialidad_codigo  
+		FROM gd_esquema.Maestra 
+		WHERE medico_nombre IS NOT NULL
+		GROUP BY medico_nombre, medico_apellido, medico_dni, medico_direccion, medico_fecha_nac, medico_mail, medico_telefono, especialidad_codigo
+	
+	OPEN Cur_Medicos 
 		
-		FETCH Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm	
-	
+		FETCH Cur_Medicos INTO @medi_nom, @medi_apell, @medi_dni, @medi_direccion, @medi_fecha_nac, @medi_mail, @medi_telefono, @medi_especialidad
+				
 		WHILE (@@FETCH_STATUS = 0)
-
+		
 			BEGIN	
 
-				INSERT INTO [GRUPOSA].[PlanesMedicos]
-				([Plan_Codigo],[Plan_Descripcion],[Plan_Precio_Bono_Consulta],[Plan_Precio_Bono_Farmacia])
-		   		VALUES
-				(@plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm);
-					
-				FETCH NEXT FROM Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm
+				SET @medi_usuario = NEXT VALUE FOR GRUPOSA.SQ_ID_USUARIO
+				
+				INSERT INTO [GRUPOSA].[Medicos]
+						   ([Medi_Nombre],[Medi_Apellido],[Medi_Dni],[Medi_Direccion],[Medi_Mail],[Medi_Telefono],[Medi_Fecha_Nac]
+						   ,[FK_Medico_Especialidad],[Medi_Usuario])
+					 VALUES
+						   (@medi_nom, @medi_apell, @medi_dni, @medi_direccion, @medi_mail, @medi_telefono, @medi_fecha_nac, @medi_especialidad, 
+						  cast(@medi_usuario as varchar(10)));
+
+				FETCH NEXT FROM Cur_Medicos INTO @medi_nom, @medi_apell, @medi_dni, @medi_direccion, @medi_fecha_nac, @medi_mail, 
+				@medi_telefono, @medi_especialidad
 			END
-		
 
-	CLOSE Cur_PlanesMedicos
+	CLOSE Cur_Medicos
 
-	DEALLOCATE Cur_PlanesMedicos
+	DEALLOCATE Cur_Medicos
 	
 COMMIT TRANSACTION
 
---===========================================================================================================
---===========================================================================================================
---/* 4 - ABM Especialidades Medicas */
 
-USE GD2C2016
-BEGIN TRANSACTION
-	
-	DECLARE @espe_cod 			NUMERIC(18,0)
-	DECLARE @espe_desc			VARCHAR(255)
-	DECLARE @espe_tipo_cod 		NUMERIC(18,0)
-	DECLARE @espe_tipo_desc		VARCHAR(255)
-
-	DECLARE Cur_Especialidades CURSOR FOR
-	
-		SELECT DISTINCT(especialidad_codigo), especialidad_descripcion, tipo_especialidad_codigo, tipo_especialidad_descripcion 
-		FROM gd_esquema.Maestra
-		WHERE especialidad_codigo IS NOT NULL
-		ORDER BY 1 ASC
-
-	OPEN Cur_Especialidades 
-		
-		FETCH Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
-		WHILE (@@FETCH_STATUS = 0)
-
-			BEGIN	
-
-				INSERT INTO [GRUPOSA].[Especialidades]
-				([Espe_Cod],[Espe_Desc],[Espe_Tipo_Cod],[Espe_Tipo_Desc])
-		   		VALUES
-				(@espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc);
-					
-				FETCH NEXT FROM Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
-			END
-		
-
-	CLOSE Cur_Especialidades
-
-	DEALLOCATE Cur_Especialidades
-	
-COMMIT TRANSACTION
-
---===========================================================================================================
---===========================================================================================================
---5 tipos de documento
-insert GRUPOSA.TipoDocumento
-VALUES (
-	CAST(0 AS NUMERIC(18, 0))
-	,N'DNI'
-	)
-GO
-insert GRUPOSA.TipoDocumento
-VALUES (
-	CAST(1 AS NUMERIC(18, 0))
-	,N'Pasaporte'
-	)
-GO
-insert GRUPOSA.TipoDocumento
-VALUES (
-	CAST(2 AS NUMERIC(18, 0))
-	,N'LC'
-	)
 
 --===========================================================================================================
 --===========================================================================================================
