7f53ba66f634d110ec08d8f8487ecaf484f2937b
Alejandro <alemattioli95@gmail.com>
2016-10-01 12:14:51 -0300

    Cambios en tablas y migracion

--- Migracion_DB/Migracion.sql
+++ Migracion_DB/Migracion.sql
@@ -39,16 +39,51 @@ SELECT DISTINCT GRUPOSA.ObtenerUserName(Medico_Nombre,Medico_Apellido,Medico_Fec
 FROM GD2C2016.gd_esquema.Maestra 
 Where Medico_Dni is not null 
 
-go
 --===========================================================================================================
 --===========================================================================================================
---3 copio planes medicos
-insert GRUPOSA.PlanesMedicos
-select distinct Plan_Med_Codigo,Plan_Med_Descripcion,Plan_Med_Precio_Bono_Consulta,Plan_Med_Precio_Bono_Farmacia
-from GD2C2016.gd_esquema.Maestra
+--/* 3 - Planes Medicos */
+
+USE GD2C2016
+BEGIN TRANSACTION
+	
+	DECLARE @plan_cod 					NUMERIC(18,0)
+	DECLARE @plan_desc					VARCHAR(255)
+	DECLARE @plan_precio_consulta 		NUMERIC(18,0)
+	DECLARE @plan_precio_bono_farm		NUMERIC(18,0)
+
+	DECLARE Cur_PlanesMedicos CURSOR FOR
+	
+		SELECT DISTINCT(plan_med_codigo), plan_med_descripcion,plan_med_precio_bono_consulta,plan_med_precio_bono_farmacia
+		FROM gd_esquema.Maestra
+		WHERE plan_med_codigo IS NOT NULL
+		ORDER BY 1 ASC
+
+	OPEN Cur_PlanesMedicos 
+		
+		FETCH Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm	
+	
+		WHILE (@@FETCH_STATUS = 0)
+
+			BEGIN	
+
+				INSERT INTO [GRUPOSA].[PlanesMedicos]
+				([Plan_Codigo],[Plan_Descripcion],[Plan_Precio_Bono_Consulta],[Plan_Precio_Bono_Farmacia])
+		   		VALUES
+				(@plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm);
+					
+				FETCH NEXT FROM Cur_PlanesMedicos INTO @plan_cod, @plan_desc, @plan_precio_consulta, @plan_precio_bono_farm
+			END
+		
+
+	CLOSE Cur_PlanesMedicos
+
+	DEALLOCATE Cur_PlanesMedicos
+	
+COMMIT TRANSACTION
+
 --===========================================================================================================
 --===========================================================================================================
-/* 4 - ABM Especialidades Medicas */
+--/* 4 - ABM Especialidades Medicas */
 
 USE GD2C2016
 BEGIN TRANSACTION
@@ -58,32 +93,32 @@ BEGIN TRANSACTION
 	DECLARE @espe_tipo_cod 		NUMERIC(18,0)
 	DECLARE @espe_tipo_desc		VARCHAR(255)
 
-	DECLARE Cur_Especialidades CURSOR FOR
+	DECLARE Cur_PlanesMedicos CURSOR FOR
 	
 		SELECT DISTINCT(especialidad_codigo), especialidad_descripcion, tipo_especialidad_codigo, tipo_especialidad_descripcion 
 		FROM gd_esquema.Maestra
 		WHERE especialidad_codigo IS NOT NULL
 		ORDER BY 1 ASC
 
-	OPEN Cur_Especialidades 
+	OPEN Cur_PlanesMedicos 
 		
-		FETCH Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
+		FETCH Cur_PlanesMedicos INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
 		WHILE (@@FETCH_STATUS = 0)
 
 			BEGIN	
 
 				INSERT INTO [GRUPOSA].[Especialidades]
-				([Especialidad_Cod],[Especialidad_Desc],[Especialidad_Tipo_Cod],[Especialidad_Tipo_Desc])
+				([Espe_Cod],[Espe_Desc],[Espe_Tipo_Cod],[Espe_Tipo_Desc])
 		   		VALUES
 				(@espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc);
 					
-				FETCH NEXT FROM Cur_Especialidades INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
+				FETCH NEXT FROM Cur_PlanesMedicos INTO @espe_cod, @espe_desc, @espe_tipo_cod, @espe_tipo_desc
 			END
 		
 
-	CLOSE Cur_Especialidades
+	CLOSE Cur_PlanesMedicos
 
-	DEALLOCATE Cur_Especialidades
+	DEALLOCATE Cur_PlanesMedicos
 	
 COMMIT TRANSACTION
 
@@ -107,7 +142,7 @@ VALUES (
 	CAST(2 AS NUMERIC(18, 0))
 	,N'LC'
 	)
-GO
+
 --===========================================================================================================
 --===========================================================================================================
 --6 tipos de rol
