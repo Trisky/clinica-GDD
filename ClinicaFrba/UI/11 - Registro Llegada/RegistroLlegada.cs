187e653e1dbdfd4a91d293ab9f1553098804daeb
Trisky <sebastian@styrsky.com.ar>
2016-10-30 20:48:51 -0300

    #Empecé con registro de llegada, va a ser facil
    #AbmAfiliado ahora tiene la habilidad de darle un afiliado al registro de llegadas pa' ahorrar codigo
    
    #Cambio en el enum de los dias, sigo sin poder hacerlo andar en visual studio 2012

---
+++ ClinicaFrba/UI/11 - Registro Llegada/RegistroLlegada.cs
@@ -0,0 +1,35 @@
+﻿using ClinicaFrba.FormulariosBase;
+using ClinicaFrba.Pedir_Turno;
+using ClinicaFrba.UI._04___Abm_Afiliado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.Registro_Llegada
+{
+    public partial class RegistroLlegada : FormBase
+    {
+        private AbmAfiliadoListar abmAfiliado;
+        private PedirTurno pedidorTurnos;
+        public RegistroLlegada()
+        {
+            InitializeComponent();
+            pedidorTurnos = new PedirTurno();
+            abmAfiliado = new AbmAfiliadoListar(this);
+        }
+
+        internal void ElTurnoEsPara(DataGridViewRow dr)
+        {
+            var cells = dr.Cells;
+            var nombre = cells[1].Value.ToString();
+            var apellido = cells[2].Value.ToString();
+            var IDAfiliado = cells[0].Value.ToString();
+        }
+    }
+}
