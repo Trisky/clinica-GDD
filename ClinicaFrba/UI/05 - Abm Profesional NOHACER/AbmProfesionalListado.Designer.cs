a3cd6e57b17cb3b25fefaa0cfeeb2ba6f6856dda
Trisky <sebastian@styrsky.com.ar>
2016-12-03 20:03:42 -0300

    listar profesionales

--- ClinicaFrba/UI/05 - Abm Profesional NOHACER/AbmProfesionalListado.Designer.cs
+++ ClinicaFrba/UI/05 - Abm Profesional NOHACER/AbmProfesionalListado.Designer.cs
@@ -28,11 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.gbFiltros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.Controls.Add(this.textBoxApellido);
+            this.gbFiltros.Controls.Add(this.textBoxNombre);
+            this.gbFiltros.Controls.Add(this.label2);
+            this.gbFiltros.Controls.Add(this.label1);
+            this.gbFiltros.Controls.SetChildIndex(this.btnBuscar, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.btnLimpiar, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label1, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label2, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.textBoxNombre, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.textBoxApellido, 0);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Visible = false;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Visible = false;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Visible = false;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(675, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "apellido";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(724, 51);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNombre.TabIndex = 4;
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(724, 76);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(100, 20);
+            this.textBoxApellido.TabIndex = 5;
+            // 
+            // AbmProfesionalListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 469);
+            this.Name = "AbmProfesionalListado";
             this.Text = "AbmProfesionalListado";
+            this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label label2;
     }
 }
--- ClinicaFrba/UI/05 - Abm Profesional NOHACER/AbmProfesionalListado.cs
+++ ClinicaFrba/UI/05 - Abm Profesional NOHACER/AbmProfesionalListado.cs
@@ -1,8 +1,12 @@
 ﻿using ClinicaFrba.FormulariosBase;
+using ClinicaFrba.Helpers;
+using ClinicaFrba.Logica.Entidades;
+using ClinicaFrba.UI.MenuPrincipal;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,9 +17,65 @@ namespace ClinicaFrba.UI._05___Abm_Profesional
 {
     public partial class AbmProfesionalListado : FormularioListadoBase
     {
-        public AbmProfesionalListado()
+        private PantallaPrincipal pantallaPP;
+
+        public AbmProfesionalListado(PantallaPrincipal pp)
         {
             InitializeComponent();
+            pantallaPP = pp;
+            Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            Conexion con = new Conexion();
+            string s = @"SELECT [Medi_Id] as matricula
+                          ,[Medi_Nombre] as nombre
+                          ,[Medi_Apellido] as apellido
+                          ,[Medi_Dni] as DNI
+                          ,[Medi_Sexo] as sexo
+                          ,[Medi_Usuario] as usuario
+                      FROM [GD2C2016].[GRUPOSA].[Medico]
+                      ";
+            if (textBoxApellido.Text == "" && textBoxNombre.Text == "")
+                dt = con.SimpleQuery(s);
+            else
+            {
+                string q = s +
+                             @"where Medi_Nombre like @nombre or
+                                     Medi_Apellido like @apellido";
+                SqlCommand cmd = con.CrearComandoQuery(q);
+                cmd.Parameters.Add(new SqlParameter("@nombre", con.ConWildCard(textBoxNombre.Text)));
+                cmd.Parameters.Add(new SqlParameter("@apellido", con.ConWildCard(textBoxApellido.Text)));
+
+                dt = con.ExecConsulta(cmd);
+            }
+        }
+
+        private void btnSeleccionar_Click_1(object sender, EventArgs e)
+        {
+            var a = dgListado.SelectedRows[0];
+            if (a != null)
+            {
+                var cells = a.Cells;
+
+                UsuarioLogeado ua = new UsuarioLogeado();
+                ua.PacienteMatricula = cells[0].Value.ToString();
+                ua.UserName = cells[4].Value.ToString();
+                pantallaPP.afiliadoSeleccionado(ua);
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar un afiliado", "Afiliado no seleccionado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            Hide();
+            Dispose();
         }
     }
 }
